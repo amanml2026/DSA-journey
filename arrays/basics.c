eca557f45dd9ed70d7d7bbd1f4e65ec8bb748cb9
Aman Khanal <meena6aman@gmail.com>
2026-07-25 14:44:14 +0530

    Made array directory and implemented some ways of initialising arrays

---
+++ arrays/basics.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int A[5]; //gives array of size 5 with garbage values
+    int B[10] = {1,2,3,4,5,6}; // gives array of size 10 with initial values given and rest 0.
+    int C[5] = {0}; // gives array of size 5 with all elements 0.
+    int D[] = {1,2,3,4}; // generate array of size 4 .
+
+    return 0;
+}
