bf686309056141af19a8ab67704956849fe4e7cb
Aman Khanal <meena6aman@gmail.com>
2026-07-28 13:26:24 +0530

    added the display function to see swapped elements

--- arrays_ADT/searching.c
+++ arrays_ADT/searching.c
@@ -11,13 +11,16 @@ struct Array{
 int LinearSearch_tr(struct Array *arr,int key); // linear search with transpose
 void swap(int *x,int *y); // swap elements
 int LinearSearch_head(struct Array *arr,int key); // linear search with key element moving to head
+void display(struct Array arr); // displaying array elements
 
 int main()
 {
     struct Array arr = {{2,4,6,8,10},10,5}; // array
     printf("%d\n",LinearSearch_tr(&arr,8)); // successful search 
+    display(arr);
     printf("%d\n",LinearSearch_tr(&arr,12)); // unsuccessful search -> returns -1
     printf("%d\n",LinearSearch_head(&arr,10)); // moving key element to head returns 0 if successful else -1.
+    display(arr);
     return 0;
 }
 
@@ -62,4 +65,16 @@ void swap(int *x,int *y)
     temp = *x;
     *x = *y;
     *y = temp;
+}
+// function to display the elements of the array:
+void display(struct Array arr)
+{
+    int i;
+
+    printf("The elements are :\n");
+    for(i=0;i<arr.length;i++)
+    {
+        printf("%d ",arr.A[i]);
+    }
+    printf("\n");
 }
