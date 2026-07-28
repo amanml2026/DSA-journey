2934b10cb9876059d33f4fdbbe17820934d4acf4
Aman Khanal <meena6aman@gmail.com>
2026-07-28 13:18:56 +0530

    improved linear search using transposing

--- arrays_ADT/searching.c
+++ arrays_ADT/searching.c
@@ -8,26 +8,43 @@ struct Array{
         int length;
 };
 
-int LinearSearch(struct Array arr,int key);
+int LinearSearch_tr(struct Array *arr,int key);
+void swap(int *x,int *y);
 
 int main()
 {
     struct Array arr = {{2,4,6,8,10},10,5}; // array
-    printf("%d\n",LinearSearch(arr,2)); // successful search -> gives index 0
-    printf("%d\n",LinearSearch(arr,12)); // unsuccessful search -> returns -1
+    printf("%d\n",LinearSearch_tr(&arr,8)); // successful search 
+    printf("%d\n",LinearSearch_tr(&arr,12)); // unsuccessful search -> returns -1
     return 0;
 }
 
-// function for searching a element using linearsearch
-int LinearSearch(struct Array arr,int key)
+// function for searching a element using linearsearch with transposing
+int LinearSearch_tr(struct Array *arr,int key) 
 {
     int i;
-    for (i = 0;i<arr.length;i++)
+    for (i = 0;i<arr->length;i++)
     {
-        if (arr.A[i] == key)
+        if (arr->A[i] == key)
         {
-            return i; // search successful
+            if (i>0)
+            {
+                swap(&arr->A[i],&arr->A[i-1]);
+                return i-1; // search successful
+            }
+            else{
+                return 0; // if key is at index 0 
+            }
         }
     }
     return -1; // search unsuccessful
+}
+
+
+void swap(int *x,int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
 }
