aec392b0138aeed32bebf237e3685f36dafcbdf6
Aman Khanal <meena6aman@gmail.com>
2026-07-29 15:13:38 +0530

    Reverse function added using two pointers method.

--- arrays_ADT/rearrangement.c
+++ arrays_ADT/rearrangement.c
@@ -9,12 +9,15 @@ struct Array{
 };
 void display(struct Array arr); // display
 void reverse_aux(struct Array *arr); // reversing using auxillary array
+void reverse_twoP(struct Array *arr); // reversing using two pointers
 
 int main()
 {
     struct Array arr = {{24,42,61,84,100},10,5}; // array
     display(arr);
-    reverse_aux(&arr); // reversing 
+    reverse_aux(&arr); // reversing using auxillary array
+    display(arr);
+    reverse_twoP(&arr); // again reversing using two pointers -> return original unchanched array
     display(arr);
     return 0;
 }
@@ -30,6 +33,19 @@ void reverse_aux(struct Array *arr)
     for(i=0;i<arr->length;i++)
     {
         arr->A[i] = B[i]; // changing the original array
+    }
+}
+
+// reversing the array using two pointers
+void reverse_twoP(struct Array *arr)
+{
+    int i,j;
+    for(i=0,j=(arr->length)-1;i<j;i++,j--)
+    {
+        // swapping
+        int temp = arr->A[i];
+        arr->A[i] = arr->A[j];
+        arr->A[j] = temp;
     }
 }
 // display array
