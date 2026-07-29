cfb478256acc11853314cd77901c46a35d20f88b
Aman Khanal <meena6aman@gmail.com>
2026-07-29 13:53:31 +0530

    Made a file arithmetic.c to implement max,min,sum,avg etc.

---
+++ arrays_ADT/arithmetic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// array struct
+struct Array{
+        int A[10];
+        int size;
+        int length;
+};
+
+int main()
+{
+    struct Array arr = {{2,4,6,8,10},10,5}; // array
+    
+
+    return 0;
+}
