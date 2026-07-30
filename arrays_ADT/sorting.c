deaee4de33e056738bc8afab50ee221f7bbbb385
Aman Khanal <meena6aman@gmail.com>
2026-07-30 15:45:43 +0530

    Implemented an algorithm to insert element in a sorted array

--- arrays_ADT/sorting.c
+++ arrays_ADT/sorting.c
@@ -11,12 +11,15 @@ struct Array{
 
 void display(struct Array arr); // displaying the array
 bool isSorted(struct Array arr);// check for sorted array
+void sortInsert(struct Array *arr,int x); // inserting element in sorted array
 
 int main()
 {
     struct Array arr = {{2,4,5,9,11,12},10,6};
     display(arr);
-    printf("%d",isSorted(arr));
+    printf("%d\n",isSorted(arr));
+    sortInsert(&arr,7); // inserting 7 in sorted array
+    display(arr);
     return 0;
 }
 
@@ -44,4 +47,27 @@ bool isSorted(struct Array arr)
         }
     }
     return true; // returns 1 for sorted array
+}
+// Insert element in sorted array
+void sortInsert(struct Array *arr,int x)
+{
+    if(arr->length<arr->size)
+    {
+        if(isSorted(*arr) == 1)
+        {
+            int i=arr->length-1;
+            while(x<arr->A[i])
+            {
+                arr->A[i+1] = arr->A[i];
+                i--;
+            }
+            arr->A[i+1] = x;
+        }
+        else{
+            printf("Array must be sorted\n ");
+        }
+    }
+    else{
+        printf("Array is full\n");
+    }
 }
