35d59cfc270cdaa92497e3e7f40eb7c6ced9204f
Aman Khanal <meena6aman@gmail.com>
2026-07-31 11:23:16 +0530

    Implemented a function to merge two sorted arrays into one merged sorted array

--- arrays_ADT/two_array_operations.c
+++ arrays_ADT/two_array_operations.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 // array struct
 struct Array{
@@ -8,6 +7,8 @@ struct Array{
         int size;
         int length;
 };
+struct Array *Merge(struct Array *arr1,struct Array *arr2); // Merging two sorted arrays
+void display(struct Array arr);
 
 int main()
 {
@@ -15,6 +16,8 @@ int main()
     struct Array arr2 = {{3,7,11,13,14,19,30},100,7}; // array2 with length 7
 
     struct Array *arr3;
+    arr3 = Merge(&arr1,&arr2);
+    display(*arr3);
 }
 
 // display function
@@ -28,4 +31,38 @@ void display(struct Array arr)
         printf("%d ",arr.A[i]);
     }
     printf("\n");
+}
+
+struct Array *Merge(struct Array *arr1,struct Array *arr2)
+{
+    int i,j,k;
+    i=j=k=0;
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+    arr3->size = 100;
+    while(i<arr1->length && j<arr2->length)
+    {
+        if(arr1->A[i]<arr2->A[j])
+        {
+            arr3->A[k] = arr1->A[i];
+            i++;
+            k++;
+        }
+        else{
+            arr3->A[k] = arr2->A[j];
+            j++;
+            k++;
+        }
+    }
+    for(;i<arr1->length;i++)
+    {
+        arr3->A[k] = arr1->A[i];
+        k++;
+    }
+    for(;j<arr2->length;j++)
+    {
+        arr3->A[k] = arr2->A[j];
+        k++;
+    }
+    arr3->length = arr1->length + arr2->length;
+    return arr3;
 }
