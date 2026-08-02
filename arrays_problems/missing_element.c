3ba93c678deefbf9cfdabac9248c4e058a12dff8
Aman Khanal <meena6aman@gmail.com>
2026-08-02 21:55:22 +0530

    added approach to solve the problem

--- arrays_problems/missing_element.c
+++ arrays_problems/missing_element.c
@@ -1,2 +1,5 @@
 /* Given a natural number array : 1,2,3,4,5,6,8,9
 Find the missing number in the sequence*/
+
+/*Approach: finding the last element and finding sum using formula n(n+1)/2 and then finding sum of the
+array and finding difference , difference = missing element*/
