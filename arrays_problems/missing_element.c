7b3b58e3a2cadf703ee50855809f3d480139b615
Aman Khanal <meena6aman@gmail.com>
2026-08-02 22:54:01 +0530

    problem where array is not starting from zero and had to find a missing element solution added

--- arrays_problems/missing_element.c
+++ arrays_problems/missing_element.c
@@ -1,8 +1,10 @@
-/* Given a natural number array : 1,2,3,4,5,6,8,9
-Find the missing number in the sequence*/
+/* 1.Given a natural number array : [1,2,3,4,5,6,8,9]
+Find the missing number in the sequence
 
-/*Approach: finding the last element and finding sum using formula n(n+1)/2 and then finding sum of the
-array and finding difference , difference = missing element*/
+2.Given an array which is not starting from 1 i.e [6,7,8,9,10,12,13,14,15]
+find missing element
+
+3.*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,16 +14,19 @@ struct Array{
     int size;
     int length;
 };
-int solution(struct Array arr);
+int solution1(struct Array arr); // missing element in a sequence of numbers starting from 1
+int solution2(struct Array arr); // missing element in a sequence of numbers starting from any number
 
 int main()
 {
-    struct Array arr = {{1,2,3,4,5,6,8,9},10,8};
-    printf("%i",solution(arr));
+    struct Array arr1 = {{1,2,3,4,5,6,8,9},10,8};
+    struct Array arr2 = {{6,7,8,9,10,12,13,14,15},10,9};
+    printf("%i\n",solution1(arr1));
+    printf("%i\n",solution2(arr2));
     return 0;
 }
 
-int solution(struct Array arr)
+int solution1(struct Array arr)
 {
     int i,n,sum,sum_actual;
     n = arr.A[arr.length-1];
@@ -36,3 +41,16 @@ int solution(struct Array arr)
 
 }
 
+int solution2(struct Array arr)
+{
+    int i,diff;
+    diff = arr.A[0]-0;
+    for(i=0;i<arr.length;i++)
+    {
+        if(arr.A[i] - i != diff)
+        {
+            return diff+i;
+            break;
+        }
+    }
+}
