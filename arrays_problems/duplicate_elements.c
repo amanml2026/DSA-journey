e46a4d9cab1ec4ea983953facaa117a24c7c7402
Aman Khanal <meena6aman@gmail.com>
2026-08-04 12:31:17 +0530

    made a file to solve problem realted to duplicate elements and made a struct Array

---
+++ arrays_problems/duplicate_elements.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Array{
+    int A[20];
+    int size;
+    int length;
+};
+/*1. find the duplicate elements in an array and print only one time.*/
+
