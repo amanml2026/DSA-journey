e1f46505035d2c49762ed4066758f3a12bcc893e
Aman Khanal <meena6aman@gmail.com>
2026-08-04 12:34:02 +0530

    problem statement

--- arrays_problems/duplicate_elements.c
+++ arrays_problems/duplicate_elements.c
@@ -6,5 +6,15 @@ struct Array{
     int size;
     int length;
 };
-/*1. find the duplicate elements in an array and print only one time.*/
+/*1. find the duplicate elements in a sorted array and print only one time.*/
+void Solution1(struct Array arr);
 
+int main()
+{
+    struct Array arr1 = {{2,3,4,4,5,6,7,7,7,8,9,9,9,9},20,14};
+}
+
+void Solution1(struct Array arr)
+{
+    
+}
