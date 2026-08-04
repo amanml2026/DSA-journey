294c56e437b0f77cec788042d28ff82af1ca2abb
Aman Khanal <meena6aman@gmail.com>
2026-08-04 12:47:26 +0530

    implemented the solution for problem 1 to find the duplicates in sorted array.

--- arrays_problems/duplicate_elements.c
+++ arrays_problems/duplicate_elements.c
@@ -12,9 +12,20 @@ void Solution1(struct Array arr);
 int main()
 {
     struct Array arr1 = {{2,3,4,4,5,6,7,7,7,8,9,9,9,9},20,14};
+    Solution1(arr1);
 }
 
 void Solution1(struct Array arr)
 {
-    
+    int last_duplicate,i;
+    last_duplicate = -1; // array has positive numbers
+    printf("The missing elements are :\n");
+    for(i=0;i<arr.length-1;i++)
+    {
+        if(arr.A[i] == arr.A[i+1] && arr.A[i] != last_duplicate)
+        {
+            printf("%d ",arr.A[i]);
+            last_duplicate = arr.A[i];
+        }
+    }
 }
