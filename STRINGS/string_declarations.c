e81e3369a15c9ada4118d6b3d666d6f16d24cded
Aman Khanal <meena6aman@gmail.com>
2026-08-05 20:09:02 +0530

    printed the strings declared by several methods

--- STRINGS/string_declarations.c
+++ STRINGS/string_declarations.c
@@ -6,6 +6,7 @@ int main()
     char city[] = {'D','u','b','a','i','\0'}; // method2
     char country[] = "India"; // method3
     char *job = "SDE"; // method4
-    
+
+    printf("%s,%s,%s,%s",name,city,country,job);
     return 0;
 }
