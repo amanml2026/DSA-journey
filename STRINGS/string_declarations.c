07a964aca631606cef23995000c2d6a41bbb50a8
Aman Khanal <meena6aman@gmail.com>
2026-08-05 20:07:01 +0530

    STRINGS folder created and made a file for different methods of declaring string

---
+++ STRINGS/string_declarations.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[5] = {'J','o','h','n','\0'}; // method1
+    char city[] = {'D','u','b','a','i','\0'}; // method2
+    char country[] = "India"; // method3
+    char *job = "SDE"; // method4
+    
+    return 0;
+}
