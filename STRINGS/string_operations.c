d2e8a92df093e5ab755170964b74085c91ef3c29
Aman Khanal <meena6aman@gmail.com>
2026-08-05 21:51:09 +0530

    implemented a function to convert uppercase to lowercase.

--- STRINGS/string_operations.c
+++ STRINGS/string_operations.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int length(char *s); // length of the string
+char *Lower(char *s); // Make the string to lower case
 int main()
 {
-    char *s = "California";
-    printf("Length of the string is %d",length(s));
+    char s[] = "CALIFORNIA";
+    printf("Length of the string is %d\n",length(s));
+    
+    printf("%s",Lower(s));
     return 0;
 }
 
 // find length of the string
-int length(char *s)
+int length(char *s)       
 {
     int i;
     i=0;
@@ -19,4 +22,17 @@ int length(char *s)
     }
     // i will stop at the index where the string delimiter is there
     return i; // i = length
+}
+
+// change the upper case to lower case given a string of upper chars only
+char *Lower(char *s)
+{
+    char *lower = s;
+    int i = 0;
+    while(s[i] != '\0')
+    {
+        s[i] += 32;
+        i++;
+    }
+    return lower;
 }
