469d8eabc6c91c32a38080df0e316a7538e3ad1b
Aman Khanal <meena6aman@gmail.com>
2026-08-05 20:13:20 +0530

    made a function to find length of the string

--- STRINGS/string_operations.c
+++ STRINGS/string_operations.c
@@ -3,4 +3,19 @@
 int main()
 {
     char *s = "California";
+
+    return 0;
+}
+
+// find length of the string
+int length(char *s)
+{
+    int i;
+    i=0;
+    while(s[i] != '\0') // iterate till the end of string character
+    {
+        i++; // incrementing i
+    }
+    // i will stop at the index where the string delimiter is there
+    return i; // i = length
 }
