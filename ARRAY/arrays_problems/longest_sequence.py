cb98d41056bf70c45026fe19da0545324af7e4a5
Aman Khanal <meena6aman@gmail.com>
2026-08-09 22:04:02 +0530

    added a new problem of longest consecutive in list

---
+++ ARRAY/arrays_problems/longest_sequence.py
@@ -0,0 +1,25 @@
+'''Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be formed.
+A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than 
+the previous element. The elements do not have to be consecutive in the original array.
+You must write an algorithm that runs in O(n) time.'''
+
+
+def longestConsecutive( nums: List[int]) -> int:
+    if len(nums) == 0:
+        return 0
+    low = min(nums)
+    high = max(nums)
+
+    frequency = [0] * (high-low+1)
+    for i in range(len(nums)):
+        frequency[nums[i]-low] += 1
+
+    i = consq = 0
+    while(i<len(frequency)):
+        j = i
+        while( j<len(frequency) and frequency[j]>0 ):
+            j += 1
+        if(j-i>consq):
+            consq = j-i
+        i = j+1
+    return consq
