7f7a9ddf2e83e2ace2a43c375381bc08b64e24c2
Aman Khanal <meena6aman@gmail.com>
2026-08-14 15:26:42 +0530

    added the approach to solve problem

--- STRINGS/string problems/group_anagrams.py
+++ STRINGS/string problems/group_anagrams.py
@@ -1,2 +1,6 @@
 '''Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.'''
 
+# Approach : 1. Making a dictionary where the key would be the frequency array and values would be the strings.
+#            2. for each string in the given list of strings finding the frequency array and storing them in dictionary with
+#               as the frequency table.
+#            3. returning the list of dictionary values.
