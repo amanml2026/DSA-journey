c43c29274223d14e2e01ad49dcc73957b1b26c24
Aman Khanal <meena6aman@gmail.com>
2026-08-15 22:11:58 +0530

    Added the approach to solve problem

--- ARRAY/arrays_problems/duplicate.py
+++ ARRAY/arrays_problems/duplicate.py
@@ -1,4 +1,5 @@
 '''
 Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
 '''
-
+# APPROACH:
+# We will just use the 'set' datatype of python and compare the length of original list and set
