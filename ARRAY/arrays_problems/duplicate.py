094dca0e2956418defb804b326bf66d7eb3a2a9f
Aman Khanal <meena6aman@gmail.com>
2026-08-15 22:17:06 +0530

    tested the function worked good

--- ARRAY/arrays_problems/duplicate.py
+++ ARRAY/arrays_problems/duplicate.py
@@ -6,3 +6,11 @@ Given an integer array nums, return true if any value appears more than once in 
 
 def hasDuplicate(nums: list[int]) -> bool:
     return (len(set(nums))!=len(nums))
+
+nums = [1,2,3,3,4]
+nums2 = [2,5,6,1,3]
+
+print(hasDuplicate(nums))
+print(hasDuplicate(nums2))
+
+
