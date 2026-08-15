f2319d086f4d7fd5c84f4e9ae2b8c21ff3188e39
Aman Khanal <meena6aman@gmail.com>
2026-08-15 22:15:47 +0530

    added the solution

--- ARRAY/arrays_problems/duplicate.py
+++ ARRAY/arrays_problems/duplicate.py
@@ -3,3 +3,6 @@ Given an integer array nums, return true if any value appears more than once in 
 '''
 # APPROACH:
 # We will just use the 'set' datatype of python and compare the length of original list and set
+
+def hasDuplicate(nums: list[int]) -> bool:
+    return (len(set(nums))!=len(nums))
