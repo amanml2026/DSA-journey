5e276b60e051efa0dd657f13aa0edaee525fb54e
Aman Khanal <meena6aman@gmail.com>
2026-08-24 22:38:33 +0530

    first implementation for the better approach.

--- ARRAY/arrays_problems/3sum.py
+++ ARRAY/arrays_problems/3sum.py
@@ -1,7 +1,8 @@
 '''Given an integer array nums, return all the triplets [nums[i], nums[j], 
 nums[k]] where nums[i] + nums[j] + nums[k] == 0, and the indices i, j and k are all distinct.'''
 
-def threeSum(nums: list[int]) -> list[list[int]]: # O(n^3) -> BRUTE FORCE 
+# BRUTE FORCE 
+def threeSum(nums: list[int]) -> list[list[int]]: # O(n^3)
     ans = [] 
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
@@ -15,3 +16,20 @@ def threeSum(nums: list[int]) -> list[list[int]]: # O(n^3) -> BRUTE FORCE
 
 nums = [-1,0,1,2,-1,-4]
 print(threeSum(nums))
+
+def threeSum_2(nums : list[int]) -> list[list[int]]:
+    ans = []
+    nums = sorted(nums)
+    for i in range(len(nums)-2):
+        j = i+1
+        k = len(nums)-1
+        while(j<k):
+            if (nums[i]+nums[j]+nums[k] == 0):
+                ans.append([nums[i],nums[j],nums[k]])
+
+            elif (nums[j]+nums[k]< -(nums[i])):
+                j += 1
+            elif (nums[j]+nums[k] > -(nums[i])):
+                k -=1 
+
+print()
