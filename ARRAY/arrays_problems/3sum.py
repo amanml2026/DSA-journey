e3305eecfaac84d2d6c75bd47f04336caa2e5ac2
Aman Khanal <meena6aman@gmail.com>
2026-08-24 22:44:56 +0530

    tested the function worked fine -> O(n^2)

--- ARRAY/arrays_problems/3sum.py
+++ ARRAY/arrays_problems/3sum.py
@@ -17,19 +17,26 @@ def threeSum(nums: list[int]) -> list[list[int]]: # O(n^3)
 nums = [-1,0,1,2,-1,-4]
 print(threeSum(nums))
 
+
 def threeSum_2(nums : list[int]) -> list[list[int]]:
     ans = []
     nums = sorted(nums)
     for i in range(len(nums)-2):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
         j = i+1
         k = len(nums)-1
         while(j<k):
             if (nums[i]+nums[j]+nums[k] == 0):
                 ans.append([nums[i],nums[j],nums[k]])
+                j += 1
+                k -= 1
 
             elif (nums[j]+nums[k]< -(nums[i])):
                 j += 1
             elif (nums[j]+nums[k] > -(nums[i])):
                 k -=1 
 
-print()
+    return ans
+
+print(threeSum_2(nums))
