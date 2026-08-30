4ac9148239f72b8dcfb75132b608d7e18529ef82
Aman Khanal <meena6aman@gmail.com>
2026-08-30 18:02:28 +0530

    added the solution

--- ARRAY/arrays_problems/Two_Integer_Sum.py
+++ ARRAY/arrays_problems/Two_Integer_Sum.py
@@ -6,3 +6,14 @@ therefore you may not use the same element twice.
 
 '''
 
+def twoSum(numbers: list[int], target: int) -> list[int]:
+    i = 0
+    j = len(numbers) - 1
+    while(i<j):
+        if(numbers[i] + numbers[j])>target:
+            j -= 1
+        elif (numbers[i] + numbers[j])<target:
+            i += 1
+        else:
+            return [i+1,j+1]
+
