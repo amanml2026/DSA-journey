3c3f11b287140252151b4b3d6272b883aa9b7d20
Aman Khanal <meena6aman@gmail.com>
2026-09-06 22:53:24 +0530

    COMMENTS

--- ARRAY/arrays_problems/valid_parantheses.py
+++ ARRAY/arrays_problems/valid_parantheses.py
@@ -11,21 +11,21 @@ Return true if s is a valid string, and false otherwise.
 '''
 
 def isValid( s: str) -> bool:
-    stack = []
-    closeOpen = {')': '(', '}':'{', ']':'['}
+    stack = [] # stack 
+    closeOpen = {')': '(', '}':'{', ']':'['} # dictionary for the opening and closing brackets
     for char in s:
-        if char in closeOpen:
-            if stack and closeOpen[char] == stack[-1]:
-                stack.pop()
+        if char in closeOpen: # if char is a closing bracket
+            if stack and closeOpen[char] == stack[-1]: # if len(stack) != 0 and closing bracket = opening bracket
+                stack.pop() 
             else:
-                return False
+                return False # len(stack) == 0
         else:
-            stack.append(char)
+            stack.append(char) # if char is opening bracket
 
-    return True if not stack else False
+    return True if not stack else False # if stack is empty -> every opening bracket got popped -> valid parantheses
 
 string1 = "([{}])"
-print(isValid(string1))
+print(isValid(string1)) # return True
 
 string2 = "[(])"
-print(isValid(string2))
+print(isValid(string2)) # return False
