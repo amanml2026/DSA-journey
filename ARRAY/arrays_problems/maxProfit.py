d3d076c117ba62d9cb51d59859503b3322ecb652
Aman Khanal <meena6aman@gmail.com>
2026-09-08 22:54:02 +0530

    comments added

--- ARRAY/arrays_problems/maxProfit.py
+++ ARRAY/arrays_problems/maxProfit.py
@@ -7,18 +7,18 @@ Return the maximum profit you can achieve. You may choose to not make any transa
 '''
 
 def maxProfit( prices: list[int]) -> int:
-    l = 0
-    r = 1
+    l = 0 # buying point
+    r = 1 # selling point
     maxP = 0
-    while(r<len(prices)):
-        if (prices[r]>prices[l]):
+    while(r<len(prices)): # bound of the pointer
+        if (prices[r]>prices[l]): # we are in profit
             profit = prices[r] - prices[l]
             maxP = max(profit,maxP)
         else:
-            l = r
+            l = r # r is the least value seen se far -> therefore we want low buying price so setting l=r
         r += 1
 
-    return maxP
-
+    return maxP # maximum profit
+# testing the function
 prices = [10,1,5,6,7,1]
-print(maxProfit(prices))
+print(maxProfit(prices)) # 6
