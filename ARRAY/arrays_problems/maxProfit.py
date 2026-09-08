b752a5643a2c1126df1ff3d32ef5abcf333df905
Aman Khanal <meena6aman@gmail.com>
2026-09-08 22:49:34 +0530

    added the solution

--- ARRAY/arrays_problems/maxProfit.py
+++ ARRAY/arrays_problems/maxProfit.py
@@ -6,3 +6,16 @@ You may choose a single day to buy one NeetCoin and choose a different day in th
 Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
 '''
 
+def maxProfit(self, prices: list[int]) -> int:
+    l = 0
+    r = 1
+    maxP = 0
+    while(r<len(prices)):
+        if (prices[r]>prices[l]):
+            profit = prices[r] - prices[l]
+            maxP = max(profit,maxP)
+        else:
+            l = r
+        r += 1
+
+    return maxP
