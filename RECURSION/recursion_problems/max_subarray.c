81e192e36cfcf95884674de3a25af1cae93bffc5
Aman Khanal <meena6aman@gmail.com>
2026-09-09 23:11:26 +0530

    tested the cases -> worked perfect.

--- RECURSION/recursion_problems/max_subarray.c
+++ RECURSION/recursion_problems/max_subarray.c
@@ -8,21 +8,23 @@ typedef struct{
     int sum;
 }info;
 
-info MaxSubarray(int A[]);
+info MaxSubarray(int A[],int len);
 
 int main()
 {
     int Test[] = {-2, 3, -1, 5, -6, 4, 2, -1, 3, -8, 7, -2, 4, -1, 2, -5, 4};
-    printf("%d,%d,%d",MaxSubarray(Test).left,MaxSubarray(Test).right,MaxSubarray(Test).sum);
+    printf("%d,%d,%d\n",MaxSubarray(Test,17).left,MaxSubarray(Test,17).right,MaxSubarray(Test,17).sum);
+    int Test2[] = {-4, 2, -3, 6, -1, 4, -7, 3, 5, -2, 4, -6, 2, 8, -3, 1, -5};
+    printf("%d,%d,%d\n",MaxSubarray(Test2,17).left,MaxSubarray(Test2,17).right,MaxSubarray(Test2,17).sum);
     return 0;
 
 }
 
 // Brute force approach
-info MaxSubarray(int A[])
+info MaxSubarray(int A[],int len)
 {
-    int len,l,r,low,high,sum,MaxSum;
-    len = sizeof(A);
+    int l,r,low,high,sum;
+    float MaxSum;
     MaxSum = -INFINITY;
 
     for(l=0;l<len;l++)
