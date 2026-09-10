a9c17a8a3619309bbac7b38f0df0bc7586d23c5e
Aman Khanal <meena6aman@gmail.com>
2026-09-10 23:09:01 +0530

    implemented the function

---
+++ RECURSION/recursion_problems/kadane_algorithm.py
@@ -0,0 +1,27 @@
+def kadane_algo(A):
+    if len(A) == 0:
+        return "NULL"
+    maxSum = float('-inf')
+    i = 0
+
+    while i < len(A):
+
+        sum = 0
+        j = i
+
+        while (j < len(A) ):
+            sum += A[j]
+            if sum > maxSum:
+                low = i
+                high = j
+                maxSum = sum
+            j += 1
+            if sum<0:
+                break
+
+        if sum<0:
+            i = j
+        if j == len(A):
+            break
+
+    return low,high,maxSum
