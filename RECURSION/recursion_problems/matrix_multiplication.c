c72e625ba09cf1d864a911a563370428879869a7
Aman Khanal <meena6aman@gmail.com>
2026-09-15 19:11:20 +0530

    function for matrix multiplication -> three nested loop

--- RECURSION/recursion_problems/matrix_multiplication.c
+++ RECURSION/recursion_problems/matrix_multiplication.c
@@ -3,17 +3,34 @@
 
 typedef struct{
     int size;
-    int M[][];
+    int M[10][10];
 }matrix;
 
 int main()
 {
-
+    
 
     return 0;
 }
 
 matrix Multiply(matrix A,matrix B)
 {
-    n = A.size
+    int n,i,j,k;
+    n = A.size;
+    matrix C;
+    C.size = n;
+
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            C.M[i][j] = 0;
+            for(k=0;k<n;k++)
+            {
+                C.M[i][j] += (A.M[i][k])*(B.M[k][j]);
+            }
+        }
+    }
+    return C;
+    
 }
