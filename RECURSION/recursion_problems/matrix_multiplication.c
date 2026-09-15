86af434ff80b4bea2855a4162fc4b054a3b879a1
Aman Khanal <meena6aman@gmail.com>
2026-09-15 19:03:17 +0530

    made a typedef struct for the matrix

--- RECURSION/recursion_problems/matrix_multiplication.c
+++ RECURSION/recursion_problems/matrix_multiplication.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+    int size;
+    int M[][];
+}matrix;
+
 int main()
 {
 
 
     return 0;
+}
+
+matrix Multiply(matrix A,matrix B)
+{
+    n = A.size
 }
