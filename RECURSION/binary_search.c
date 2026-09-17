09dfb354b468bbb6a9cb9284b0a1df050551a34e
Aman Khanal <meena6aman@gmail.com>
2026-09-17 22:49:11 +0530

    implemented non-recursive (loop) version of binary search

--- RECURSION/binary_search.c
+++ RECURSION/binary_search.c
@@ -1,1 +1,32 @@
-#include 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    return 0;
+}
+
+int Search(int A[],int len,int target)
+{
+    int l,h,mid;
+    l = 0;
+    h = len - 1;
+
+    while (l <= h)
+    {
+        mid = (l+h)/2;
+        if (target == A[mid])
+        {
+            return mid;
+        }
+        else if(target > A[mid])
+        {
+            l = mid + 1;
+        }
+        else{
+            h = mid - 1;
+        }
+    }
+    return -1;
+}
